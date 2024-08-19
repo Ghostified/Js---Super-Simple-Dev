42249e0246b4821ea8fdf1f48d55c573d2594f26
Ghostified <bransonallan@gmail.com>
2024-08-20 00:30:18 +0300

    lesson 5 complete

---
+++ script.js
@@ -0,0 +1,42 @@
+// 'My name is'
+// let myVar = 'My name is '
+// console.log(myVar)
+// let myName = 'Branson'
+// console.log(myVar + myName)
+// let totalCost ='The total cost is:$ ' + (3 + 5);
+// console.log(totalCost)
+
+/// calculating total with template strings and interpolation 
+// let total=`The total cost is:$ ${(3+5)}`
+// console.log(total) 
+
+// let coffe = (5.99*100)
+// let bagel = (2.95 *100)
+// let output = 'The total cost:$ '
+// console.log(output + (coffe + bagel)/100)
+// let total2 = `Total Cost: $ ${(coffe + bagel)/100}`
+// console.log(total2)
+// alert(total2 +    `\nThank you come again!`)
+
+        console.log(Math.round((2095 + 71) * 0.1) /100);
+        console.log((25 *9/5)+ 32);
+        console.log((86-32)*(5/9));
+
+        //Strings
+        'Hello'
+        //alert('hello' + ' there');
+        console.log('hello ' + 'there');
+        typeof 'hello'
+        console.log(typeof 'hello');
+        console.log('Total amount is $: ' + Math.round((20.95*100) +(7.99 * 100))/100);
+        console.log ('Items ('+ (1 +1) + '): $ ' + (2095 + 799)/100);
+        console.log("I'm Learning Java Script")
+
+        //Escape Sequences
+        console.log('I\'m learning  \n javascript')
+        console.log(`Hello Template strings`)
+
+        //Interpolation = insert a character directly into a strings
+        console.log(`Items (${ 1+ 1}): $${(2095 + 799) /100}`);
+        console.log(`some
+            text`)
